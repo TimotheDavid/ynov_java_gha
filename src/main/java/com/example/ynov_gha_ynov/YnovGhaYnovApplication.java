f182041cf20e4f71303f65c4825b40798584baa9
Timothe David <timoth.david@gmail.com>
2024-04-26 10:09:18 +0200

    init

---
+++ src/main/java/com/example/ynov_gha_ynov/YnovGhaYnovApplication.java
@@ -0,0 +1,13 @@
+package com.example.ynov_gha_ynov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnovGhaYnovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YnovGhaYnovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ynov_gha_ynov/YnovGhaYnovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ynov_gha_ynov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnovGhaYnovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
